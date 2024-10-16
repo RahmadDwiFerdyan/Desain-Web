8e0c6fefa1ab00633fc2406cb28d7ba70c2680bf
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2024-10-16 14:27:05 +0700

    n/3/4/5/6/7

--- minggu8/upload.php
+++ minggu8/upload.php
@@ -3,12 +3,25 @@
 if(isset($_POST["submit"])){
     $targetdir = "uploads/";
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
+    
+    //modifnya
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+    $maxsize = 3*1024*1024;
 
-    if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
-        echo "File berhasil diunggah bos.";
+    if(in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"]<=$maxsize){
+
+        if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
+            echo "File berhasil diunggah bos.";
+            echo "<br><img src='$targetfile' style='width: 200px; height: auto;'>";
+        } else {
+            echo "Gagal gagal gagal";
+        }
+
     } else {
-        echo "Gagal gagal gagal";
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
     }
 }
+    
 
 ?>
